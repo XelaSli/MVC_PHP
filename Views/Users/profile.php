2947dfc6eadbc9e0072a74f849ddf4378928bfff
Arnaud <arnaud.sintes@coding-academy.fr>
2018-07-30 23:51:13 +0200

    creation of Profile.php to display user info

---
+++ Views/Users/profile.php
@@ -0,0 +1,14 @@
+<?php $title = "Profile"; ?>
+
+<?php ob_start(); ?>
+<form method="get">
+    <label for="username">Username:   </label>
+    <input type="text" id="username" name="username" placeholder="username" readonly>
+    <br><br>
+    <label for="email">Email:   </label>
+    <input type="email" id="email" name="email" placeholder="email" readonly>
+</form>
+<?php $content = ob_get_clean(); ?>
+
+<?php require_once("template.php");
+?>
