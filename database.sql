d9af4be5f2c5bc16b505cdf0fdbcfb5963d03623
Arnaud <arnaud.sintes@coding-academy.fr>
2018-07-30 22:55:36 +0200

    creation base de donnees

---
+++ database.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS db_PHP_Rush_MVC;
+CREATE DATABASE db_PHP_Rush_MVC;
+USE db_PHP_Rush_MVC;
+
+
+CREATE TABLE `users` (
+  `id` SERIAL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `edition_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE NOW()
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
