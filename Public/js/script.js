8f1b95adab6c1228be083d956ccb8f46c35fca40
Benjamin Sibellas <benjamin.sibellas@coding-academy.fr>
2018-07-31 12:17:40 +0200

    added script.js to remove the menu for registration and connexion

---
+++ Public/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var noMenu = $("#noMenu");
+    if (noMenu.length !== 0) {
+        $("header").hide();
+    }
+}
--- Views/Users/template.php
+++ Views/Users/template.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <title><?= $title?></title>
+    <script type="text/javascript" href="../../Vendors/jquery.min.js" />
+    <script type="text/javascript" href="../../Public/js/script.js" />"
 </head>
 <body>
 <header><?php include_once("menu.php"); ?></header>
