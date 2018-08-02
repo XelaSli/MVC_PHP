a8e666a6f25384c9d08ebca0264e1049094b48df
Benjamin Sibellas <benjamin.sibellas@coding-academy.fr>
2018-08-02 15:11:06 +0200

    added links to authors, tags, categories and dates in blog

--- Views/Articles/blog.php
+++ Views/Articles/blog.php
@@ -27,19 +27,19 @@ foreach ($articleList as $article) {
     ?>
 <h2><?=$article["title"]?></h2>
 <p><?=$article["content"]?></p>
-<p><em>Written by <a href="UsersController.php?filter=<?=$article["user_id"] ?>&amp;type=Author"><?=$userController::getUser()->display_user($article["user_id"])["username"]?></a> on <?=$article["creation_date"]?></em></p>
+<p><em>Written by <a href="UsersController.php?filter=<?=$article["user_id"] ?>&amp;type=Author"><?=$userController::getUser()->display_user($article["user_id"])["username"]?></a> on <a href="UsersController.php?filter=<?=$article["creation_date"] ?>&amp;type=Date"><?=$article["creation_date"]?></a></em></p>
 <?php
 if ($article["creation_date"] != $article["edition_date"]) {
         echo "<p><em>Last modification: " . $article["edition_date"] . "</em></p>";
     }
     ?>
-    <p>Category: <?=$category_object->getCategory($article["category_id"])?></p>
+    <p>Category: <a href="UsersController.php?filter=<?=$article["category_id"]?>&amp;type=Category"><?=$category_object->getCategory($article["category_id"])?></a></p>
 <?php
 $tags = $tags_object->getArticleTags($article["id"]);
     if ($tags != false) {
         echo "Tags: ";
         foreach ($tags as $tag) {
-            echo $tag["tag"] . " ";
+            echo "<a href='UsersController.php?filter=" . $tags_object->getTagId($tag["tag"]) . "&amp;type=Tag'>" . $tag["tag"] . "</a> ";
         }
     }
     if ($_SESSION["group"] != "User") {?>
