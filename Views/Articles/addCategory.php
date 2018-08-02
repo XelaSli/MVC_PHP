e07d1585d0953516d7a4a10f10a0e47397c36703
Benjamin Sibellas <benjamin.sibellas@coding-academy.fr>
2018-08-02 16:11:27 +0200

    fixed an issue with get action create_category

--- Views/Articles/addCategory.php
+++ Views/Articles/addCategory.php
@@ -1,11 +1,11 @@
 <?php $title = "Add a category"; ?>
 
-
 <?php ob_start();?>
-<h1>Add an category</h1>
+<h1>Add a category</h1>
 <form method="post" action="ArticlesController.php">
 <label for="category_name">Name: </label>
 <input type="text" id="category_name" name="category_name" required /><br /><br />
 <br /><br />
+<?php $content = ob_get_clean();?>
 
 <?php require_once "../Views/template.php";
