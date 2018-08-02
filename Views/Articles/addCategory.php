229b2bf821c72e2424b98304dc5b99d5520e0615
Alex <alexandre.sahli@coding-academy.fr>
2018-08-02 16:13:03 +0200

    ammend blog

--- Views/Articles/addCategory.php
+++ Views/Articles/addCategory.php
@@ -4,8 +4,8 @@
 <?php ob_start();?>
 <h1>Add an category</h1>
 <form method="post" action="ArticlesController.php">
-<label for="category_article">Name: </label>
-<input type="text" id="category_article" name="category_article" required /><br /><br />
+<label for="category_name">Name: </label>
+<input type="text" id="category_name" name="category_name" required /><br /><br />
 <br /><br />
 
 <?php require_once "../Views/template.php";
