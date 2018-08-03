ee12ba13ae604d267c00fce5ec3c3a638ff6dbf6
Benjamin Sibellas <benjamin.sibellas@coding-academy.fr>
2018-08-03 16:06:49 +0200

    category name is displayed when editing it

--- Views/Articles/editCategory.php
+++ Views/Articles/editCategory.php
@@ -4,11 +4,11 @@
 <h1>Edit a category</h1>
 <form method="post" action="./edit_category&amp;id=<?= $_GET["id"]?>">
 <label for="category">Category name:   </label>
-<input type="text" id="category" name="category" value="<?php echo $cats['category'] ?>" placeholder="name" required>
+<input type="text" id="category" name="category" value="<?= $category_object->getCategory($_GET["id"]) ?>" required>
 <br><br>
 <button  type="submit" class="waves-effect blue darken-1 btn" name="submit">Modify</button>
 </form><br>
-<button  type="button" class="waves-effect blue darken-1 btn" onclick="location.href='./manage_category'">Back</button>
+<button  type="button" class="waves-effect blue darken-1 btn" onclick="location.href='./manage_category'">Back</button><br /><br />
 <?php $content = ob_get_clean(); ?>
 
 <?php require_once ("Views/template.php");?>
